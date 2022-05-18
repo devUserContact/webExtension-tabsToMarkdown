e608effebeadec7ec4efdd5e87cc66095b05e1dc
devUserContact <developer.user.contact@protonmail.com>
2022-05-18 15:15:20 -0700

    inital commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function listTabs() {
+	browser.tabs.query({currentWindow: true})
+	.then(tabs => {
+		for (const tab of tabs) {
+			console.log(tab.title, tab.url)
+		}
+	})
+}
+
+window.onload = function () {
+  const btn = document.getElementById("exeButton");
+  btn.onclick = listTabs;
+};
