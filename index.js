6a76cb15889025ef59be0ab73912dc8032414adf
devUserContact <developer.user.contact@protonmail.com>
2022-05-18 15:27:25 -0700

    formatted code with prettier

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function listTabs() {
-	browser.tabs.query({currentWindow: true})
-	.then(tabs => {
-		for (const tab of tabs) {
-			console.log(tab.title, tab.url)
-		}
-	})
+  browser.tabs.query({ currentWindow: true }).then((tabs) => {
+    for (const tab of tabs) {
+      console.log(tab.title, tab.url);
+    }
+  });
 }
 
 window.onload = function () {
