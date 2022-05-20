7cc2c38c0321d05de498a594cd8abd3dd9fa8860
devUserContact <developer.user.contact@protonmail.com>
2022-05-20 14:43:35 -0700

    added  background script for shortcut command

---
+++ background.js
@@ -0,0 +1,7 @@
+import { listTabs } from "./index.js";
+
+browser.commands.onCommand.addListener(function (command) {
+  if (command === "convert-tabs-to-markdown") {
+    listTabs();
+  }
+});
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function listTabs() {
+export function listTabs() {
   let tabsArray = [];
   let counter = 0;
   browser.tabs.query({ currentWindow: true }).then((tabs) => {
     for (const tab of tabs) {
-      counter++
+      counter++;
       tabsArray.push({
         key: `[${tab.title}][${counter}]`,
         value: `[${counter}]:[${tab.url}]`,
@@ -25,8 +25,8 @@ window.onload = function () {
   const btn = document.getElementById("exeButton");
   btn.onclick = listTabs;
 };
-browser.commands.onCommand.addListener(function (command) {
-  if (command === "convert-tabs-to-markdown") {
-    listTabs();
-  }
-});
+//browser.commands.onCommand.addListener(function (command) {
+//  if (command === "convert-tabs-to-markdown") {
+//    listTabs();
+//  }
+//});
