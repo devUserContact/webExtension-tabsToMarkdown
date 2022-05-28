e5faca35bf76dc2becc586dbb661e65a2437e34a
devUserContact <developer.user.contact@protonmail.com>
2022-05-27 23:17:33 -0700

    functional dark mode

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
 export function listTabs() {
-  //get tab data from browser and input into dictionary array
-  let tabsArray = [];
-  let counter = 0;
-  browser.tabs.query({ currentWindow: true }).then((tabs) => {
-    for (const tab of tabs) {
-      counter++;
-      tabsArray.push({
-        key: `[${tab.title}][${counter}]`,
-        value: `[${counter}]:${tab.url}`,
-        id: counter,
-      });
-    }
+	//get tab data from browser and input into dictionary array
+	let tabsArray = [];
+	let counter = 0;
+	browser.tabs.query({ currentWindow: true }).then((tabs) => {
+		for (const tab of tabs) {
+			counter++;
+			tabsArray.push({
+				key: `[${tab.title}][${counter}]`,
+				value: `[${counter}]:${tab.url}`,
+				id: counter,
+			});
+		}
 
-    // creating the markdown string
-    let tabsMarkdown = [];
-    const docTitle = document.getElementById("titleInput").value;
-    tabsMarkdown.push(`# ${docTitle}`);
-    for (var tabEntry of tabsArray) {
-      if (tabEntry.id === tabsArray.length) {
-        tabsMarkdown.push(`${tabEntry.key}`);
-      } else {
-        tabsMarkdown.push(`${tabEntry.key}\n`);
-      }
-    }
-    tabsMarkdown.push("___");
-    for (var tabEntry of tabsArray) {
-      tabsMarkdown.push(tabEntry.value);
-    }
-    let tabsMarkdownComplete = tabsMarkdown.join("\n");
+		// creating the markdown string
+		let tabsMarkdown = [];
+		const docTitle = document.getElementById("titleInput").value;
+		tabsMarkdown.push(`# ${docTitle}`);
+		for (var tabEntry of tabsArray) {
+			if (tabEntry.id === tabsArray.length) {
+				tabsMarkdown.push(`${tabEntry.key}`);
+			} else {
+				tabsMarkdown.push(`${tabEntry.key}\n`);
+			}
+		}
+		tabsMarkdown.push("___");
+		for (var tabEntry of tabsArray) {
+			tabsMarkdown.push(tabEntry.value);
+		}
+		let tabsMarkdownComplete = tabsMarkdown.join("\n");
 
-    // creating md file and text blob
-    let dlLink = document.createElement("a");
-    dlLink.download = `${docTitle}.md`;
-    let markdownDoc = new Blob([tabsMarkdownComplete], {
-      type: "text/plain",
-    });
-    dlLink.href = URL.createObjectURL(markdownDoc);
-    dlLink.click();
-  });
+		// creating md file and text blob
+		let dlLink = document.createElement("a");
+		dlLink.download = `${docTitle}.md`;
+		let markdownDoc = new Blob([tabsMarkdownComplete], {
+			type: "text/plain",
+		});
+		dlLink.href = URL.createObjectURL(markdownDoc);
+		dlLink.click();
+	});
 }
 function setIcon() {
-  if (matchMedia("(prefers-color-scheme: dark)").matches) {
-    browser.browserAction.setIcon({ path: "/icons/icon-dark32.png" });
-  } else {
-    browser.browserAction.setIcon({ path: "/icons/icon32.png" });
-  }
+	if (matchMedia("(prefers-color-scheme: dark)").matches) {
+		browserAction.setIcon({ "path": "/icons/icon-dark32.png" });
+	} else {
+		browserAction.setIcon({ "path": "/icons/icon32.png" });
+	}
 }
 window.onload = function () {
-  const btn = document.getElementById("exeButton");
-  btn.onclick = listTabs;
+	const btn = document.getElementById("exeButton");
+	btn.onclick = listTabs;
 };
 
 document.addEventListener("DOMContentLoaded", setIcon);
