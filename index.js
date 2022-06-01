80fd32f854089f4a24c2eefe223bde67adaf963f
devUserContact <developer.user.contact@protonmail.com>
2022-06-01 16:38:15 -0700

    removed light/dark mode functionality in favor of new icon version

--- index.js
+++ index.js
@@ -1,55 +1,46 @@
 export function listTabs() {
-	let tabsArray = [];
-	let counter = 0;
-	browser.tabs.query({ currentWindow: true }).then((tabs) => {
-		for (const tab of tabs) {
-			counter++;
-			tabsArray.push({
-				key: `[${tab.title}][${counter}]`,
-				value: `[${counter}]:${tab.url}`,
-				id: counter,
-			});
-		}
-		let tabsMarkdown = [];
-		let docTitle = document.getElementById("titleInput").value;
-		let currentDate = `${
-			new Date().getMonth() + 1
-		}-${new Date().getDate()}-${new Date().getFullYear()}`;
-		if (docTitle === "") {
-			docTitle = `tabsToMarkdown_${currentDate}`;
-		}
-		tabsMarkdown.push(`# ${docTitle}`);
-		for (var tabEntry of tabsArray) {
-			if (tabEntry.id === tabsArray.length) {
-				tabsMarkdown.push(`${tabEntry.key}`);
-			} else {
-				tabsMarkdown.push(`${tabEntry.key}\n`);
-			}
-		}
-		tabsMarkdown.push("___");
-		for (var tabEntry of tabsArray) {
-			tabsMarkdown.push(tabEntry.value);
-		}
-		let tabsMarkdownComplete = tabsMarkdown.join("\n");
-		let dlLink = document.createElement("a");
-		dlLink.download = `${docTitle}.md`;
-		let markdownDoc = new Blob([tabsMarkdownComplete], {
-			type: "text/plain",
-		});
-		dlLink.href = URL.createObjectURL(markdownDoc);
-		dlLink.click();
-	});
-}
-function setIcon() {
-	if (matchMedia("(prefers-color-scheme: dark)").matches) {
-		browserAction.setIcon({ path: "/icons/icon-dark48.png" });
-	} else {
-		browserAction.setIcon({ path: "/icons/icon48.png" });
-	}
+  let tabsArray = [];
+  let counter = 0;
+  browser.tabs.query({ currentWindow: true }).then((tabs) => {
+    for (const tab of tabs) {
+      counter++;
+      tabsArray.push({
+        key: `[${tab.title}][${counter}]`,
+        value: `[${counter}]:${tab.url}`,
+        id: counter,
+      });
+    }
+    let tabsMarkdown = [];
+    let docTitle = document.getElementById("titleInput").value;
+    let currentDate = `${
+      new Date().getMonth() + 1
+    }-${new Date().getDate()}-${new Date().getFullYear()}`;
+    if (docTitle === "") {
+      docTitle = `tabsToMarkdown_${currentDate}`;
+    }
+    tabsMarkdown.push(`# ${docTitle}`);
+    for (var tabEntry of tabsArray) {
+      if (tabEntry.id === tabsArray.length) {
+        tabsMarkdown.push(`${tabEntry.key}`);
+      } else {
+        tabsMarkdown.push(`${tabEntry.key}\n`);
+      }
+    }
+    tabsMarkdown.push("___");
+    for (var tabEntry of tabsArray) {
+      tabsMarkdown.push(tabEntry.value);
+    }
+    let tabsMarkdownComplete = tabsMarkdown.join("\n");
+    let dlLink = document.createElement("a");
+    dlLink.download = `${docTitle}.md`;
+    let markdownDoc = new Blob([tabsMarkdownComplete], {
+      type: "text/plain",
+    });
+    dlLink.href = URL.createObjectURL(markdownDoc);
+    dlLink.click();
+  });
 }
 window.onload = function () {
-	const btn = document.getElementById("exeButton");
-	btn.onclick = listTabs;
-	document.addEventListener("DOMContentLoaded", setIcon);
-	setIcon();
+  const btn = document.getElementById("exeButton");
+  btn.onclick = listTabs;
 };
